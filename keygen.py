4920eb4a56ed7592dc0b6ef3bb9cea5c346f4380
gabliltraydev <99366409+gabliltraydev@users.noreply.github.com>
2023-10-23 00:00:01 +0200

    guez enft

--- keygen.py
+++
@@ -1,9 +0,0 @@
-import random
-
-randint_key = random.randint(0,19378286477653757395947392597)
-encrypt_key = randint_key
-enc_key_str = str(encrypt_key)
-
-with open("encrypt_key.txt", 'w') as f:
-    f.write(enc_key_str)
-
--- pwdencrypter.py
+++
@@ -1,6 +0,0 @@
-actual_passwd = input("Quel est vôtre MDP actuel ? : ")
-
-with open("encrypt_key.txt.txt", 'r') as f:
-    encryption_key = f.read()
-
-encryption_key.replace("a", encryption_key)
